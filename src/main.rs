b2058b7adc444e32a7609f32262687d04c7364e8
amatgil <amatgilvinyes@gmail.com>
2024-06-13 03:38:40 +0200

    Added framerate, I love math

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use macroquad::{prelude::*, text};
 #[macroquad::main(window_conf)]
 async fn main() {
     // Variables
-    let background_color = Color::from_rgba(24, 25, 38, 255);
-    let grid_thickness: f32 = 2.5;
-    let grid_color = Color::from_rgba(138, 173, 244, 255);
-    let grid_spacing: usize = 40;
-    let alive_color = Color::from_rgba(145, 215, 227, 255); // Lavander
-    let dead_color = Color::from_rgba(0, 0, 0, 0); // Transparent
-    let text_color = Color::from_rgba(198, 160, 246, 200);
-    let mut time_between_ticks = 0.3; // In seconds;
-    let time_between_ticks_delta = 0.01; // In seconds
+    let background_color         = Color::from_rgba(24, 25, 38, 255);
+    let grid_thickness           = 2.5;
+    let grid_color               = Color::from_rgba(138, 173, 244, 255);
+    let grid_spacing             = 40;
+    let alive_color              = Color::from_rgba(145, 215, 227, 255); // Lavander
+    let dead_color               = Color::from_rgba(0, 0, 0, 0);         // Transparent
+    let text_color               = Color::from_rgba(198, 160, 246, 200);
+    let mut time_between_ticks   = 0.3;                                  // In seconds;
+    let time_between_ticks_delta = 0.01;                                 // In seconds
 
     // Simulation
     let mut universe = Universe::new(screen_width() as usize / grid_spacing, screen_height() as usize / grid_spacing);
@@ -38,30 +38,31 @@ async fn main() {
         } else { frames_since_last_tick += 1; }
 
         if is_key_pressed(KeyCode::Space)      { paused = !paused; }
-        if is_key_down(KeyCode::W)      {
+        if is_key_down(KeyCode::D)      {
             time_between_ticks += time_between_ticks_delta;
         }
-        if is_key_down(KeyCode::Q) {
+        if is_key_down(KeyCode::U) {
             time_between_ticks = (time_between_ticks - time_between_ticks_delta).max(0.0);
         }
 
         draw_universe(&universe, grid_spacing, alive_color, dead_color);
         draw_grid(grid_thickness, grid_color, grid_spacing);
-        draw_controls(text_color);
-
-        println!("{}", time_between_ticks);
+        draw_controls(text_color, time_between_ticks);
 
         next_frame().await
     }
 }
 
-fn draw_controls(text_color: Color) {
+fn draw_controls(text_color: Color, time_between_ticks: f32) {
+    let uni_fps = (time_between_ticks + 1.0) / (1.0/get_fps() as f32 + time_between_ticks);
+
     draw_rectangle(0.0, 0.0,
-                    320.0, 40.0*3.0,
+                    320.0, 40.0*5.0,
                     Color::from_rgba(0, 0, 0, 200));
-    draw_text("Q: Increase Speed", 10.0, 30.0 + 0.0*40.0, 40.0, text_color);
-    draw_text("W: Decrease Speed", 10.0, 30.0 + 1.0*40.0, 40.0, text_color);
-    draw_text("Space: Pause",      10.0, 30.0 + 2.0*40.0, 40.0, text_color);
+    draw_text("U: Increase Speed",              10.0, 30.0 + 0.0*40.0, 40.0, text_color);
+    draw_text("D: Decrease Speed",              10.0, 30.0 + 1.0*40.0, 40.0, text_color);
+    draw_text("Space: Pause",                   10.0, 30.0 + 2.0*40.0, 40.0, text_color);
+    draw_text(&format!("Speed: {uni_fps:.4} fps",),    10.0, 30.0 + 4.0*40.0, 40.0, text_color);
 }
 
 fn draw_universe(universe: &Universe, grid_spacing: usize, alive_color: Color, dead_color: Color) {
